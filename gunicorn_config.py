4df5112da88187fb27f9c17c71ea560bb5608fd9
Harrison Blake <harrison.r.blake@gmail.com>
2024-12-18 21:23:40 -0500

    testing hosting settings

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -8,10 +8,4 @@ workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8000')
 
-if os.getenv('ENV') == 'production':
-    debug = False
-    loglevel = 'info'
-else:
-    debug = True
-    loglevel = 'debug'
-    
+forwarded_allow_ips = '*'
